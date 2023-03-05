5ae1a8085d291ab9602c4419ce83da6478d88a1c
khoabug <khoa.nd@tinasoft.vn>
2023-03-05 22:33:38 +0700

    LAB_ClassMain

---
+++ src/LAB_ClassMain.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class LAB_ClassMain  {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numTests = scanner.nextInt();
+        scanner.nextLine(); // đọc bỏ dòng trống sau khi đọc số lượng test
+
+        for (int i = 0; i < numTests; i++) {
+            String line = scanner.nextLine();
+            int numChars = line.length();
+            int numWords = line.split("\\s+").length;
+            System.out.printf("%d %d%n", numChars, numWords);
+        }
+
+        scanner.close();
+    }
+}
